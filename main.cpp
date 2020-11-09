ccd8e48b0b5e53da67057fb7e45eb33c4c2d6cc4
WieWierz <62114839+WieWierz@users.noreply.github.com>
2020-11-09 09:54:59 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int a,b;
+	
+	cout<<"Podaj a: ";
+	cin>>a;
+	cout<<"Podaj b: ";
+	cin>>b;
+
+	cout<<"a: "<<a<<"("<< bitset<8>(a)<<")"<<endl;
+	cout<<"b: "<<b<<"("<< bitset<8>(b)<<")"<<endl;
+	
+	cout<<"a^b:("<<bitset<8>(a^b)<<")"<<endl;
+	cout<<"a|b:("<<bitset<8>(a|b)<<")"<<endl;
+	cout<<"a&b:("<<bitset<8>(a&b)<<")"<<endl;
+	cout<<"~a:("<<bitset<8>(~a)<<")"<<endl;
+	cout<<"~b:("<<bitset<8>(~b)<<")"<<endl;
+	cout<<"a <<=1:("<<bitset<8>(a <<=1)<<")"<<endl;
+	cout<<"b >>=1:("<<bitset<8>(b >>=1)<<")";
+	
+	return 0;
+}
