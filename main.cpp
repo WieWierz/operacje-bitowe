118760d9f6db062910e58f569fb9676ef84a2430
WieWierz <62114839+WieWierz@users.noreply.github.com>
2020-11-09 10:08:04 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,13 +15,12 @@ int main(int argc, char** argv) {
 	cout<<"a: "<<a<<"("<< bitset<8>(a)<<")"<<endl;
 	cout<<"b: "<<b<<"("<< bitset<8>(b)<<")"<<endl;
 	
-	cout<<"a^b:("<<bitset<8>(a^b)<<")"<<endl;
-	cout<<"a|b:("<<bitset<8>(a|b)<<")"<<endl;
-	cout<<"a&b:("<<bitset<8>(a&b)<<")"<<endl;
+	cout<<"a^b:"<<(a^b)<<"("<<bitset<8>(a^b)<<")"<<endl;
+	cout<<"a|b:"<<(a|b)<<"("<<bitset<8>(a|b)<<")"<<endl;
+	cout<<"a&b:"<<(a&b)<<"("<<bitset<8>(a&b)<<")"<<endl;
 	cout<<"~a:("<<bitset<8>(~a)<<")"<<endl;
 	cout<<"~b:("<<bitset<8>(~b)<<")"<<endl;
 	cout<<"a <<=1:("<<bitset<8>(a <<=1)<<")"<<endl;
-	cout<<"b >>=1:("<<bitset<8>(b >>=1)<<")";
-	
+	cout<<"b >>=1:("<<bitset<8>(b >>=1)<<")"<<endl;
 	return 0;
 }
